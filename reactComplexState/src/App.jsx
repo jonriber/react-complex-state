303ac3114e72571a6a11c65ce8d9713ec795fe77
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-12 12:07:56 +0100

    starting to implement the useCoupon Function

--- reactComplexState/src/App.jsx
+++ reactComplexState/src/App.jsx
@@ -2,29 +2,48 @@ import { useState } from 'react'
 import './App.css'
 import AddDiscount from './components/AddDiscount'
 
-const initialState={
+const initialState=[{
+  id:0,
   customer:"",
   code: "promo",
   used: false,
   left: 4 
-}
+},]
 
 function App() {
-  const [discount, setDiscount] = useState(initialState)
+  const 
+    [discountData, setDiscountData] = useState(initialState),
+    [idCounter,setIdCounter] = useState(1);
 
-  function applyCoupon(){
-    
+  function createCoupon(e,name){
+    let newValue = idCounter +1
+    setIdCounter(newValue)
+    e.preventDefault()
+    setDiscountData(prevState => {
+      return  [...prevState,{ customer:name, id: newValue}]
+    })
   }
 
-  console.log("discount:",discount)
+  function useCoupon(id)=>{
+    setDiscountData(prevState => {
+      return [...prevState,]
+    })
+  }
+
+  console.log("discount:",discountData)
   return (
     <>
-      <h1>React Complex State</h1>
+      <h1>React Complex State Usage Example</h1>
+
+      <h2>Following scenario</h2>
+      <p>You want to register for a dinner coupon.</p>
+      <p>Insert your name and register as an eligible customer.</p>
+
       <div className="card">
-       <AddDiscount setDiscount={setDiscount}/>
+       <AddDiscount createCoupon={createCoupon}/>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Results: {idCounter} and     
       </p>
     </>
   )
--- reactComplexState/src/components/AddDiscount.jsx
+++ reactComplexState/src/components/AddDiscount.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
 
-const AddDiscount = ({setDiscount}) =>{
+const AddDiscount = ({createCoupon}) =>{
     const [coupon, setCoupon] = useState("")
 
     return(
         <>
-            <form onSubmit={() => setDiscount(coupon)}>
+            <h3>Register for a free coupon:</h3>
+            <form onSubmit={(e) => createCoupon(e,coupon)}>
                 <fieldset>
-                    <label>Insert your coupon:</label>
-                    <input type="text" value={coupon} onChange={(e) =>setCoupon(e.target.value)}/>
+                    <label>Your name:</label>
+                    <input placeholder="Name" type="text" value={coupon} onChange={(e) =>setCoupon(e.target.value)}/>
                 </fieldset>
-                <button type="submit">Use</button>
+                <button type="submit">Register</button>
             </form>
         </>
     )
